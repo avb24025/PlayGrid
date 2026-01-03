1696ab191d1231f12b0d5b0a94d84af942d29377
avb24025 <avb24022005@gmail.com>
2026-01-03 16:18:20 +0530

    final commit

--- Backend/agent.js
+++ Backend/agent.js
@@ -6,9 +6,7 @@ import { searchTurfs } from "./nodes/searchTurfs.js";
 import { selectTurf } from "./nodes/selectTurf.js";
 import { checkAvailability } from "./nodes/checkAvailability.js";
 import { selectSlot } from "./nodes/selectSlot.js";
-import { confirmBooking } from "./nodes/confirmBooking.js";
 import { createBooking } from "./nodes/createBooking.js";
-import { initiatePayment } from "./nodes/initiatePayment.js";
 import { routeByIntent } from "./routeByIntent.js";
 import { generalAnswer } from "./nodes/generalAnswer.js";
 
@@ -22,15 +20,12 @@ graph.addNode("BookTurf",selectTurf);
 graph.addNode("CheckSlot",selectTurf);
 graph.addNode("checkAvailability", checkAvailability);
 graph.addNode("selectSlot", selectSlot);
-// graph.addNode("confirmBooking", confirmBooking);
 graph.addNode("createBooking", createBooking);
-// graph.addNode("initiatePayment", initiatePayment);
 
 graph.addEdge(START, "detectIntent");
 graph.addConditionalEdges("detectIntent", routeByIntent);
 graph.addEdge("CheckSlot","checkAvailability");
 graph.addEdge("BookTurf","selectSlot");
 graph.addEdge("selectSlot", "createBooking");
-// graph.addEdge("selectTurf", "checkAvailability");
 
 export const playgridAgent = graph.compile();
